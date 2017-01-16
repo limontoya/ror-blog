89c36e6832e6d15e0a199d5d175c389d6409c6d3
Lina J Montoya <montoyalinaj@gmail.com>
2017-01-15 19:19:04 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :posts do
+  	resources :comments, :only => :create
+  end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    'login'  => 'user_sessions#new'
+  post   'login'  => 'user_session#create'
+  delete 'logout' => 'user_sessions#destroy'
+end
